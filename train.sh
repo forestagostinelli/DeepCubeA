4ce8f31fc113d130d09ab0afc7c7d5082aeae3f6
fagostin@uci.edu <fagostin@uci.edu>
2020-06-09 20:58:32 -0700

    Updated training commands

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 
 ###### Train cost-to-go function
 python ctg_approx/avi.py --env cube3 --states_per_update 50000000 --batch_size 10000 --nnet_name cube3 --max_itrs 1000000 --loss_thresh 0.06 --back_max 30 --num_update_procs 30
-cp -r saved_models/cube3/current/* saved_models/cube3/target/
+cp -r saved_models/cube3/current/* saved_models/cube3/target/  # manually update target network
 python ctg_approx/avi.py --env cube3 --states_per_update 50000000 --batch_size 10000 --nnet_name cube3 --max_itrs 1200000 --loss_thresh 0.06 --back_max 30 --num_update_procs 30
 
 ###### Solve with A* search, use --verbose for more information
