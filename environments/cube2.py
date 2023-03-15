eeb959c8d8b671c4cc5fb1933e0f53fb43887d9a
terry feng <tzfeng@ucsd.edu>
2023-03-14 21:34:32 -0700

    create cube2

---
+++ environments/cube2.py
@@ -0,0 +1,334 @@
+from typing import List, Dict, Tuple, Union
+import numpy as np
+from torch import nn
+from random import randrange
+
+from utils.pytorch_models import ResnetModel
+from environments.environment_abstract import Environment, State
+
+
+class Cube2State(State):
+    __slots__ = ['colors', 'hash']
+
+    def __init__(self, colors: np.ndarray):
+        self.colors: np.ndarray = colors
+        self.hash = None
+
+    def __hash__(self):
+        if self.hash is None:
+            self.hash = hash(self.colors.tostring())
+
+        return self.hash
+
+    def __eq__(self, other):
+        return np.array_equal(self.colors, other.colors)
+
+
+class Cube2(Environment):
+    # Moves are in pairs: (move, -1) followed by (move, 1).
+    moves: List[str] = ["%s%i" % (f, n) for f in ['U', 'D', 'L', 'R', 'B', 'F'] for n in [-1, 1]]
+
+    def __init__(self):
+        super().__init__()
+        self.dtype = np.uint8
+        self.cube_len = 3
+
+        # solved state
+        self.goal_colors: np.ndarray = np.arange(0, (self.cube_len ** 2) * 6, 1, dtype=self.dtype)
+        # 2x2 solved state, only corners
+        self.corner_tiles_to_check = [i for i in range(54) if i % 9 == 0 or i % 9 == 2 or i % 9 == 6 or i % 9 == 8]
+
+        # get idxs changed for moves
+        self.rotate_idxs_new: Dict[str, np.ndarray]
+        self.rotate_idxs_old: Dict[str, np.ndarray]
+
+        self.adj_faces: Dict[int, np.ndarray]
+        self._get_adj()
+
+        self.rotate_idxs_new, self.rotate_idxs_old = self._compute_rotation_idxs(self.cube_len, self.moves)
+
+        # Dictionaries for corners and edges with their corresponding tiles:
+        self.corner_tiles = {
+            "WBO": [2,20,44],
+            "WBR": [0,26,47],
+            "WGO": [8,35,38],
+            "WGR": [6,29,53],
+            "YBO": [9,18,42],
+            "YBR": [11,24,45],
+            "YGO": [15,33,36],
+            "YGR": [17,27,51],
+        }
+        self.edge_tiles = {
+            "WB": [1,23],
+            "WG": [7,32],
+            "WO": [5,41],
+            "WR": [3,50],
+            "YB": [10,21],
+            "YG": [16,30],
+            "YO": [12,39],
+            "YR": [14,48],
+            "BO": [19,43],
+            "BR": [25,46],
+            "GO": [34,37],
+            "GR": [28,52],
+        }
+
+    def next_state(self, states: List[Cube2State], action: int) -> Tuple[List[Cube2State], List[float]]:
+        states_np = np.stack([x.colors for x in states], axis=0)
+        states_next_np, transition_costs = self._move_np(states_np, action)
+
+        states_next: List[Cube2State] = [Cube2State(x) for x in list(states_next_np)]
+
+        return states_next, transition_costs
+
+    def prev_state(self, states: List[Cube2State], action: int) -> List[Cube2State]:
+        move_rev_idx = action + 1 if action % 2 == 0 else action - 1
+        return self.next_state(states, move_rev_idx)[0]
+
+    def generate_goal_states(self, num_states: int, np_format: bool = False) -> Union[List[Cube2State], np.ndarray]:
+        if np_format:
+            goal_np: np.ndarray = np.expand_dims(self.goal_colors.copy(), 0)
+            solved_states: np.ndarray = np.repeat(goal_np, num_states, axis=0)
+        else:
+            solved_states: List[Cube2State] = [Cube2State(self.goal_colors.copy()) for _ in range(num_states)]
+
+        return solved_states
+
+    def is_solved(self, states: List[Cube2State]) -> np.ndarray:
+        states_np = np.stack([state.colors for state in states], axis=0)
+        #is_equal = np.equal(states_np, np.expand_dims(self.goal_colors, 0))
+        # Check only the corners for a 2x2
+        is_equal = np.equal(states_np[:, self.corner_tiles_to_check], np.expand_dims(self.goal_colors[self.corner_tiles_to_check], 0))
+
+        return np.all(is_equal, axis=1)
+
+    def state_to_nnet_input(self, states: List[Cube2State]) -> List[np.ndarray]:
+        states_np = np.stack([state.colors for state in states], axis=0)
+
+        representation_np: np.ndarray = states_np / (self.cube_len ** 2)
+        representation_np: np.ndarray = representation_np.astype(self.dtype)
+
+        representation: List[np.ndarray] = [representation_np]
+
+        return representation
+
+    def get_num_moves(self) -> int:
+        return len(self.moves)
+
+    def get_nnet_model(self) -> nn.Module:
+        state_dim: int = (self.cube_len ** 2) * 6
+        nnet = ResnetModel(state_dim, 6, 5000, 1000, 4, 1, True)
+
+        return nnet
+
+    def generate_states(self, num_states: int, backwards_range: Tuple[int, int]) -> Tuple[List[Cube2State], List[int]]:
+        """
+        Create a list of cube states, for each, scramble them with up to backwards_range[1]+1 moves
+        @param num_states create how many cubes
+        @param backwards_range (min, max) number of turns to scramble
+        """
+
+        assert (num_states > 0)
+        assert (backwards_range[0] >= 0)
+        assert self.fixed_actions, "Environments without fixed actions must implement their own method"
+
+        # Initialize
+        scrambs: List[int] = list(range(backwards_range[0], backwards_range[1] + 1))
+        num_env_moves: int = self.get_num_moves()
+
+        # Get goal states (solved cubes)
+        states_np: np.ndarray = self.generate_goal_states(num_states, np_format=True)
+
+        # Scrambles
+        scramble_nums: np.array = np.random.choice(scrambs, num_states) # how many moves to scramble per cube
+        num_back_moves: np.array = np.zeros(num_states) # how many turns have been made currently
+
+        # Go backward from goal state
+        moves_lt = num_back_moves < scramble_nums
+        while np.any(moves_lt):
+            # Get which indices still need to be scrambled further
+            idxs: np.ndarray = np.where(moves_lt)[0] 
+            # We will only scramble up to len(idxs) / 12 (moves)
+            subset_size: int = int(max(len(idxs) / num_env_moves, 1))
+            # Pick random indices to scramble
+            idxs: np.ndarray = np.random.choice(idxs, subset_size)
+
+            # Select random move from move list
+            move: int = randrange(num_env_moves)
+            # All idx selected cubes will make this turn
+            states_np[idxs], _ = self._move_np(states_np[idxs], move)
+
+            # Update how many turns have been made
+            num_back_moves[idxs] = num_back_moves[idxs] + 1
+            # Update which cubes still need to be scrambled 
+            moves_lt[idxs] = num_back_moves[idxs] < scramble_nums[idxs]
+
+        states: List[Cube2State] = [Cube2State(x) for x in list(states_np)]
+
+        return states, scramble_nums.tolist()
+
+    def expand(self, states: List[State]) -> Tuple[List[List[State]], List[np.ndarray]]:
+        assert self.fixed_actions, "Environments without fixed actions must implement their own method"
+
+        # initialize
+        num_states: int = len(states)
+        num_env_moves: int = self.get_num_moves()
+
+        states_exp: List[List[State]] = [[] for _ in range(len(states))]
+
+        tc: np.ndarray = np.empty([num_states, num_env_moves])
+
+        # numpy states
+        states_np: np.ndarray = np.stack([state.colors for state in states])
+
+        # for each move, get next states, transition costs, and if solved
+        move_idx: int
+        move: int
+        for move_idx in range(num_env_moves):
+            # next state
+            states_next_np: np.ndarray
+            tc_move: List[float]
+            states_next_np, tc_move = self._move_np(states_np, move_idx)
+
+            # transition cost
+            tc[:, move_idx] = np.array(tc_move)
+
+            for idx in range(len(states)):
+                states_exp[idx].append(Cube2State(states_next_np[idx]))
+
+        # make lists
+        tc_l: List[np.ndarray] = [tc[i] for i in range(num_states)]
+
+        return states_exp, tc_l
+
+    # For fully solved top face, transition = 1-gamma, for completely unsolved top face, transition = 1
+    def transition_costs_solvetop(self, states_np: np.ndarray, gamma: int = 0.9) -> np.ndarray:
+        corner_names = ["WBO", "WBR", "WGO", "WGR"]
+        edge_names = ["WB", "WG", "WO", "WR"]
+        # Lists of corners and edges to verify
+        corner_tiles_to_check = []
+        edge_tiles_to_check = []
+        for corner_name in corner_names:
+            corner_tiles_to_check += self.corner_tiles[corner_name]
+        for edge_name in edge_names:
+            edge_tiles_to_check += self.edge_tiles[edge_name]
+        corner_is_equal = np.equal(states_np[:, corner_tiles_to_check], np.expand_dims(self.goal_colors[corner_tiles_to_check], 0))
+        edge_is_equal = np.equal(states_np[:, edge_tiles_to_check], np.expand_dims(self.goal_colors[edge_tiles_to_check], 0))
+        return gamma/2*(np.sum(corner_is_equal, axis=1)/12 + np.sum(edge_is_equal, axis=1)/8)
+
+    def _move_np(self, states_np: np.ndarray, action: int):
+        """
+        For the cubes in states_np, they turn will turn via action
+        Args:
+            states_np: cube states to be turned
+            action: the turn that all cube states will make
+        Returns:
+            Return the next state for the cubes
+        """
+        action_str: str = self.moves[action]
+
+        states_next_np: np.ndarray = states_np.copy()
+        # Turn all cubes via the dictionary move function
+        # old idx -> new idx per cube
+        '''
+        e.g. action_str = "U"
+        print(self.rotate_idxs_old[action_str])
+        print(self.rotate_idxs_new[action_str])
+        -
+        [ 2  5  8  8  7  6  6  3  0  0  1  2 38 41 44 20 23 26 47 50 53 29 32 35]
+        [ 0  1  2  2  5  8  8  7  6  6  3  0 20 23 26 47 50 53 29 32 35 38 41 44]
+        '''
+        states_next_np[:, self.rotate_idxs_new[action_str]] = states_np[:, self.rotate_idxs_old[action_str]]
+
+        # Transition cost for a turn is 1
+        # transition_costs: List[float] = [1.0]*states_np.shape[0] #TODO: maybe this needs to be changed
+
+        # Transition cost based on whether top is solved
+        transition_costs: List[float] = 1 + self.transition_costs_solvetop(states_next_np) - self.transition_costs_solvetop(states_np)
+
+        return states_next_np, transition_costs
+
+    def _get_adj(self) -> None:
+        # WHITE:0, YELLOW:1, BLUE:2, GREEN:3, ORANGE: 4, RED: 5
+        self.adj_faces: Dict[int, np.ndarray] = {0: np.array([2, 5, 3, 4]),
+                                                 1: np.array([2, 4, 3, 5]),
+                                                 2: np.array([0, 4, 1, 5]),
+                                                 3: np.array([0, 5, 1, 4]),
+                                                 4: np.array([0, 3, 1, 2]),
+                                                 5: np.array([0, 2, 1, 3])
+                                                 }
+
+    def _compute_rotation_idxs(self, cube_len: int,
+                               moves: List[str]) -> Tuple[Dict[str, np.ndarray], Dict[str, np.ndarray]]:
+        rotate_idxs_new: Dict[str, np.ndarray] = dict()
+        rotate_idxs_old: Dict[str, np.ndarray] = dict()
+
+        for move in moves:
+            f: str = move[0]
+            sign: int = int(move[1:])
+
+            rotate_idxs_new[move] = np.array([], dtype=int)
+            rotate_idxs_old[move] = np.array([], dtype=int)
+
+            colors = np.zeros((6, cube_len, cube_len), dtype=np.int64)
+            colors_new = np.copy(colors)
+
+            # WHITE:0, YELLOW:1, BLUE:2, GREEN:3, ORANGE: 4, RED: 5
+
+            adj_idxs = {0: {2: [range(0, cube_len), cube_len - 1], 3: [range(0, cube_len), cube_len - 1],
+                            4: [range(0, cube_len), cube_len - 1], 5: [range(0, cube_len), cube_len - 1]},
+                        1: {2: [range(0, cube_len), 0], 3: [range(0, cube_len), 0], 4: [range(0, cube_len), 0],
+                            5: [range(0, cube_len), 0]},
+                        2: {0: [0, range(0, cube_len)], 1: [0, range(0, cube_len)],
+                            4: [cube_len - 1, range(cube_len - 1, -1, -1)], 5: [0, range(0, cube_len)]},
+                        3: {0: [cube_len - 1, range(0, cube_len)], 1: [cube_len - 1, range(0, cube_len)],
+                            4: [0, range(cube_len - 1, -1, -1)], 5: [cube_len - 1, range(0, cube_len)]},
+                        4: {0: [range(0, cube_len), cube_len - 1], 1: [range(cube_len - 1, -1, -1), 0],
+                            2: [0, range(0, cube_len)], 3: [cube_len - 1, range(cube_len - 1, -1, -1)]},
+                        5: {0: [range(0, cube_len), 0], 1: [range(cube_len - 1, -1, -1), cube_len - 1],
+                            2: [cube_len - 1, range(0, cube_len)], 3: [0, range(cube_len - 1, -1, -1)]}
+                        }
+            face_dict = {'U': 0, 'D': 1, 'L': 2, 'R': 3, 'B': 4, 'F': 5}
+            face = face_dict[f]
+
+            faces_to = self.adj_faces[face]
+            if sign == 1:
+                faces_from = faces_to[(np.arange(0, len(faces_to)) + 1) % len(faces_to)]
+            else:
+                faces_from = faces_to[(np.arange(len(faces_to) - 1, len(faces_to) - 1 + len(faces_to))) % len(faces_to)]
+
+            cubes_idxs = [[0, range(0, cube_len)], [range(0, cube_len), cube_len - 1],
+                          [cube_len - 1, range(cube_len - 1, -1, -1)], [range(cube_len - 1, -1, -1), 0]]
+            cubes_to = np.array([0, 1, 2, 3])
+            if sign == 1:
+                cubes_from = cubes_to[(np.arange(len(cubes_to) - 1, len(cubes_to) - 1 + len(cubes_to))) % len(cubes_to)]
+            else:
+                cubes_from = cubes_to[(np.arange(0, len(cubes_to)) + 1) % len(cubes_to)]
+
+            for i in range(4):
+                idxs_new = [[idx1, idx2] for idx1 in np.array([cubes_idxs[cubes_to[i]][0]]).flatten() for idx2 in
+                            np.array([cubes_idxs[cubes_to[i]][1]]).flatten()]
+                idxs_old = [[idx1, idx2] for idx1 in np.array([cubes_idxs[cubes_from[i]][0]]).flatten() for idx2 in
+                            np.array([cubes_idxs[cubes_from[i]][1]]).flatten()]
+                for idxNew, idxOld in zip(idxs_new, idxs_old):
+                    flat_idx_new = np.ravel_multi_index((face, idxNew[0], idxNew[1]), colors_new.shape)
+                    flat_idx_old = np.ravel_multi_index((face, idxOld[0], idxOld[1]), colors.shape)
+                    rotate_idxs_new[move] = np.concatenate((rotate_idxs_new[move], [flat_idx_new]))
+                    rotate_idxs_old[move] = np.concatenate((rotate_idxs_old[move], [flat_idx_old]))
+
+            # Rotate adjacent faces
+            face_idxs = adj_idxs[face]
+            for i in range(0, len(faces_to)):
+                face_to = faces_to[i]
+                face_from = faces_from[i]
+                idxs_new = [[idx1, idx2] for idx1 in np.array([face_idxs[face_to][0]]).flatten() for idx2 in
+                            np.array([face_idxs[face_to][1]]).flatten()]
+                idxs_old = [[idx1, idx2] for idx1 in np.array([face_idxs[face_from][0]]).flatten() for idx2 in
+                            np.array([face_idxs[face_from][1]]).flatten()]
+                for idxNew, idxOld in zip(idxs_new, idxs_old):
+                    flat_idx_new = np.ravel_multi_index((face_to, idxNew[0], idxNew[1]), colors_new.shape)
+                    flat_idx_old = np.ravel_multi_index((face_from, idxOld[0], idxOld[1]), colors.shape)
+                    rotate_idxs_new[move] = np.concatenate((rotate_idxs_new[move], [flat_idx_new]))
+                    rotate_idxs_old[move] = np.concatenate((rotate_idxs_old[move], [flat_idx_old]))
+
+        return rotate_idxs_new, rotate_idxs_old
